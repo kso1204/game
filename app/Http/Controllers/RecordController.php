a349d7502986ba458f033ff14055346072a8306c
kso1204 <kso12041204@naver.com>
2020-11-20 13:30:15 +0900

    getRecord

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -10,9 +10,10 @@ class RecordController extends Controller
     //
     public function getRecord()
     {
-        $user = User::class;
-
-        return $user->records;
-        //return DB::table('records')->with('users')->orderBy('score','desc')->take(10)->get();
+        
+        /* 
+        return DB::table('records')
+        ->leftJoin('users','users.id','=','records.user_id')
+        ->orderBy('score','desc')->take(10)->get(); */
     }
 }
