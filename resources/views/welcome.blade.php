343226cc53e5626ed3e02c2ab0db6b73470dbf57
kso1204 <kso12041204@naver.com>
2020-12-04 10:31:50 +0900

    Rule Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,12 +46,13 @@
                 </div>
 
                <div class="container">
-                   <h1>Welcome!! Are you Ready??</h1>
                    <h1>게임 목표 : 100번의 액션으로 높은 점수(Exp)를 획득하기</h1>
-                   <h1>공격하면 hp가 랜덤으로 깎이고 exp가 랜덤으로 오릅니다. ( 반격하는 허수아비)</h1>
+                   <h1>공격하면 Hp가 랜덤으로 깎이고 Exp가 랜덤으로 오릅니다. </h1>
+                   <h1>게임은 Stage에서 진행됩니다.</h1>
                    <h1>점수는 Ranking에서 확인 가능합니다.</h1>
-                   <h1>Attack < Q < W < E < R 순으로 소모hp와 획득 exp가 커집니다.</h1>
+                   <h1>Attack < Q < W < E < R 순으로 소모Hp와 획득 Exp가 커집니다.</h1>
                    <h1>죽으면 레벨이 다운 됩니다. 점수가 깎입니다.</h1>
+                   <h1>적절한 전략으로 고득점을 획득해보세요</h1>
                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
                </div>
